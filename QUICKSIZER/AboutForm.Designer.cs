4e61406ca6c22aa6e85649b7f79c7d5957805c28
mluksevics <45032922+mluksevics@users.noreply.github.com>
2019-01-10 16:46:36 +0200

    Update of "about" section. v.0.03

--- QUICKSIZER/AboutForm.Designer.cs
+++ QUICKSIZER/AboutForm.Designer.cs
@@ -33,7 +33,6 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
@@ -50,6 +49,18 @@
             this.label19 = new System.Windows.Forms.Label();
             this.label20 = new System.Windows.Forms.Label();
             this.label21 = new System.Windows.Forms.Label();
+            this.label22 = new System.Windows.Forms.Label();
+            this.label23 = new System.Windows.Forms.Label();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.label24 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label25 = new System.Windows.Forms.Label();
+            this.label26 = new System.Windows.Forms.Label();
+            this.label27 = new System.Windows.Forms.Label();
+            this.label28 = new System.Windows.Forms.Label();
+            this.label29 = new System.Windows.Forms.Label();
+            this.label30 = new System.Windows.Forms.Label();
+            this.label31 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -65,11 +76,11 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(143, 53);
+            this.label2.Location = new System.Drawing.Point(164, 53);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(63, 13);
             this.label2.TabIndex = 1;
-            this.label2.Text = "v0.02 alpha";
+            this.label2.Text = "v0.03 alpha";
             // 
             // label3
             // 
@@ -99,16 +110,6 @@
             this.label5.TabIndex = 4;
             this.label5.Text = "Although I have my best intentions in developing this program.";
             // 
-            // linkLabel1
-            // 
-            this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(191, 31);
-            this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(105, 13);
-            this.linkLabel1.TabIndex = 5;
-            this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "marcis@structures.lv";
-            // 
             // label6
             // 
             this.label6.AutoSize = true;
@@ -122,7 +123,7 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(12, 133);
+            this.label7.Location = new System.Drawing.Point(12, 289);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(227, 13);
             this.label7.TabIndex = 7;
@@ -131,7 +132,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(12, 149);
+            this.label8.Location = new System.Drawing.Point(12, 305);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(273, 13);
             this.label8.TabIndex = 8;
@@ -140,7 +141,7 @@
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(12, 195);
+            this.label9.Location = new System.Drawing.Point(12, 351);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(270, 13);
             this.label9.TabIndex = 9;
@@ -149,7 +150,7 @@
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(12, 211);
+            this.label10.Location = new System.Drawing.Point(12, 367);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(223, 13);
             this.label10.TabIndex = 10;
@@ -158,7 +159,7 @@
             // label11
             // 
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(12, 179);
+            this.label11.Location = new System.Drawing.Point(12, 335);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(215, 13);
             this.label11.TabIndex = 11;
@@ -167,7 +168,7 @@
             // label12
             // 
             this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(12, 273);
+            this.label12.Location = new System.Drawing.Point(12, 418);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(211, 13);
             this.label12.TabIndex = 14;
@@ -176,7 +177,7 @@
             // label13
             // 
             this.label13.AutoSize = true;
-            this.label13.Location = new System.Drawing.Point(12, 257);
+            this.label13.Location = new System.Drawing.Point(12, 402);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(273, 13);
             this.label13.TabIndex = 13;
@@ -186,7 +187,7 @@
             // 
             this.label14.AutoSize = true;
             this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label14.Location = new System.Drawing.Point(12, 241);
+            this.label14.Location = new System.Drawing.Point(12, 386);
             this.label14.Name = "label14";
             this.label14.Size = new System.Drawing.Size(321, 13);
             this.label14.TabIndex = 12;
@@ -195,16 +196,16 @@
             // label15
             // 
             this.label15.AutoSize = true;
-            this.label15.Location = new System.Drawing.Point(12, 164);
+            this.label15.Location = new System.Drawing.Point(12, 320);
             this.label15.Name = "label15";
-            this.label15.Size = new System.Drawing.Size(262, 13);
+            this.label15.Size = new System.Drawing.Size(284, 13);
             this.label15.TabIndex = 15;
-            this.label15.Text = "-) Resistances are based on buckling about weak axis";
+            this.label15.Text = "-) Resistances are based on buckling about weak axis (z-z)";
             // 
             // label16
             // 
             this.label16.AutoSize = true;
-            this.label16.Location = new System.Drawing.Point(12, 289);
+            this.label16.Location = new System.Drawing.Point(12, 434);
             this.label16.Name = "label16";
             this.label16.Size = new System.Drawing.Size(397, 13);
             this.label16.TabIndex = 16;
@@ -214,7 +215,7 @@
             // label17
             // 
             this.label17.AutoSize = true;
-            this.label17.Location = new System.Drawing.Point(22, 303);
+            this.label17.Location = new System.Drawing.Point(22, 448);
             this.label17.Name = "label17";
             this.label17.Size = new System.Drawing.Size(166, 13);
             this.label17.TabIndex = 17;
@@ -223,7 +224,7 @@
             // label18
             // 
             this.label18.AutoSize = true;
-            this.label18.Location = new System.Drawing.Point(12, 317);
+            this.label18.Location = new System.Drawing.Point(12, 462);
             this.label18.Name = "label18";
             this.label18.Size = new System.Drawing.Size(320, 13);
             this.label18.TabIndex = 18;
@@ -232,16 +233,16 @@
             // label19
             // 
             this.label19.AutoSize = true;
-            this.label19.Location = new System.Drawing.Point(41, 330);
+            this.label19.Location = new System.Drawing.Point(41, 475);
             this.label19.Name = "label19";
             this.label19.Size = new System.Drawing.Size(291, 13);
             this.label19.TabIndex = 19;
-            this.label19.Text = "-) deflection check assumes uniformly distirbuted load (UDL);";
+            this.label19.Text = "-) deflection check assumes uniformly distributed load (UDL);";
             // 
             // label20
             // 
             this.label20.AutoSize = true;
-            this.label20.Location = new System.Drawing.Point(41, 345);
+            this.label20.Location = new System.Drawing.Point(41, 490);
             this.label20.Name = "label20";
             this.label20.Size = new System.Drawing.Size(374, 13);
             this.label20.TabIndex = 20;
@@ -250,17 +251,142 @@
             // label21
             // 
             this.label21.AutoSize = true;
-            this.label21.Location = new System.Drawing.Point(41, 361);
+            this.label21.Location = new System.Drawing.Point(41, 506);
             this.label21.Name = "label21";
             this.label21.Size = new System.Drawing.Size(327, 13);
             this.label21.TabIndex = 21;
             this.label21.Text = "-) Moment shape for M.Rd calculations is assumed to be a parabola;";
             // 
+            // label22
+            // 
+            this.label22.AutoSize = true;
+            this.label22.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label22.Location = new System.Drawing.Point(12, 124);
+            this.label22.Name = "label22";
+            this.label22.Size = new System.Drawing.Size(289, 13);
+            this.label22.TabIndex = 22;
+            this.label22.Text = "All this little program does - process existing data:";
+            // 
+            // label23
+            // 
+            this.label23.AutoSize = true;
+            this.label23.Location = new System.Drawing.Point(12, 137);
+            this.label23.Name = "label23";
+            this.label23.Size = new System.Drawing.Size(261, 13);
+            this.label23.TabIndex = 23;
+            this.label23.Text = "Steel design data from the following sources are used:";
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(12, 150);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(196, 13);
+            this.linkLabel2.TabIndex = 24;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "https://www.steelforlifebluebook.co.uk/";
+            // 
+            // label24
+            // 
+            this.label24.AutoSize = true;
+            this.label24.Location = new System.Drawing.Point(194, 31);
+            this.label24.Name = "label24";
+            this.label24.Size = new System.Drawing.Size(105, 13);
+            this.label24.TabIndex = 25;
+            this.label24.Text = "marcis@structures.lv";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(12, 163);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(182, 13);
+            this.linkLabel1.TabIndex = 26;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "http://orangebook.arcelormittal.com/";
+            // 
+            // label25
+            // 
+            this.label25.AutoSize = true;
+            this.label25.Location = new System.Drawing.Point(13, 177);
+            this.label25.Name = "label25";
+            this.label25.Size = new System.Drawing.Size(330, 13);
+            this.label25.TabIndex = 27;
+            this.label25.Text = "Timber design data is calculated by myself, according to EN1995-1-1";
+            // 
+            // label26
+            // 
+            this.label26.AutoSize = true;
+            this.label26.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label26.Location = new System.Drawing.Point(13, 197);
+            this.label26.Name = "label26";
+            this.label26.Size = new System.Drawing.Size(130, 13);
+            this.label26.TabIndex = 28;
+            this.label26.Text = "You are the engineer:";
+            // 
+            // label27
+            // 
+            this.label27.AutoSize = true;
+            this.label27.Location = new System.Drawing.Point(13, 212);
+            this.label27.Name = "label27";
+            this.label27.Size = new System.Drawing.Size(352, 13);
+            this.label27.TabIndex = 29;
+            this.label27.Text = "-) Check the availability of sections in the local market before using those;";
+            // 
+            // label28
+            // 
+            this.label28.AutoSize = true;
+            this.label28.Location = new System.Drawing.Point(13, 226);
+            this.label28.Name = "label28";
+            this.label28.Size = new System.Drawing.Size(200, 13);
+            this.label28.TabIndex = 30;
+            this.label28.Text = "-) Some of sections in tables are Class 4; ";
+            // 
+            // label29
+            // 
+            this.label29.AutoSize = true;
+            this.label29.Location = new System.Drawing.Point(13, 241);
+            this.label29.Name = "label29";
+            this.label29.Size = new System.Drawing.Size(404, 13);
+            this.label29.TabIndex = 31;
+            this.label29.Text = "-) Have a think about connections. Is the section size or wall thickness big enou" +
+    "gh? ";
+            // 
+            // label30
+            // 
+            this.label30.AutoSize = true;
+            this.label30.Location = new System.Drawing.Point(13, 255);
+            this.label30.Name = "label30";
+            this.label30.Size = new System.Drawing.Size(344, 13);
+            this.label30.TabIndex = 32;
+            this.label30.Text = "-) Tool selects sections for pure compression of pure bending elements. ";
+            // 
+            // label31
+            // 
+            this.label31.AutoSize = true;
+            this.label31.Location = new System.Drawing.Point(24, 268);
+            this.label31.Name = "label31";
+            this.label31.Size = new System.Drawing.Size(377, 13);
+            this.label31.TabIndex = 33;
+            this.label31.Text = "It is your task to judge whether combination of axial/bending might be relevant.";
+            // 
             // AboutForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(434, 389);
+            this.ClientSize = new System.Drawing.Size(437, 535);
+            this.Controls.Add(this.label31);
+            this.Controls.Add(this.label30);
+            this.Controls.Add(this.label29);
+            this.Controls.Add(this.label28);
+            this.Controls.Add(this.label27);
+            this.Controls.Add(this.label26);
+            this.Controls.Add(this.label25);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label24);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.label23);
+            this.Controls.Add(this.label22);
             this.Controls.Add(this.label21);
             this.Controls.Add(this.label20);
             this.Controls.Add(this.label19);
@@ -277,7 +403,6 @@
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -300,7 +425,6 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.LinkLabel linkLabel1;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
@@ -317,5 +441,17 @@
         private System.Windows.Forms.Label label19;
         private System.Windows.Forms.Label label20;
         private System.Windows.Forms.Label label21;
+        private System.Windows.Forms.Label label22;
+        private System.Windows.Forms.Label label23;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.Label label24;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label25;
+        private System.Windows.Forms.Label label26;
+        private System.Windows.Forms.Label label27;
+        private System.Windows.Forms.Label label28;
+        private System.Windows.Forms.Label label29;
+        private System.Windows.Forms.Label label30;
+        private System.Windows.Forms.Label label31;
     }
 }
